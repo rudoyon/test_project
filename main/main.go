80f633dd4c71fb4357eaedfe55fb82559216c43a
rudoyon <oleg02071988@gmail.com>
2025-10-02 13:28:15 +0300

    create main

---
+++ main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Print("Starting the service...")
+	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprint(w, "Hello! Your request was processed")
+	},
+	)
+
+	router := handlers.Router()
+	log.print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
